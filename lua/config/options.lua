bfe697ed00ebc3d66231c7523b2dee0899e0de82
ali-afk <ali.hussain.ali.oun@gmail.com>
2025-08-31 14:35:55 +0300

    still have no idea what's going on...

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Add any additional options before
+
+vim.g.lazyvim_picker = "fzf"
+vim.g.lazyvim_cmp = "blink.cmp"
+
+vim.opt.expandtab = true
+vim.opt.grepprg = "ugrep -RInk -j -u --tabs=1 --ignore-files"
+vim.opt.grepformat = "%f:%l:%c:%m,%f+%l+%c+%m,%-G%f|%l|%c|%m"
